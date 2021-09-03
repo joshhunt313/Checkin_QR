cbb6cbceec9b3152bd0fbd26b9ce157c339e13bd
= <joshhruns@gmail.com>
2021-09-03 10:55:06 -0400

    Got rid of some magic #s

--- genQR.py
+++ genQR.py
@@ -4,8 +4,10 @@ from os import path
 from openpyxl import load_workbook
 import glob
 
-# Get user input
 # Note: program assumes first row is just column titles
+FIRST_ROW = 2
+
+# Get user input
 FILENAME = input('Name of excel file: ')
 WB_TITLE = input('Workbook title: ')
 QR_COL = int(input('Column # to make QRs: '))
@@ -23,7 +25,7 @@ else:
     os.mkdir('QRs')
 
 # Read through the specified column and create the unique QRs
-for col in ws.iter_cols(min_row=2, min_col=QR_COL, max_col=QR_COL):
+for col in ws.iter_cols(min_row=FIRST_ROW, min_col=QR_COL, max_col=QR_COL):
     for cell in col:
         qr = pyqrcode.create(cell.value)
         with open('QRs/' + cell.value + '.png', 'wb') as fstream:
